0de262b9719238530a022c51fa112cfcb65eb75d
Prayog <ppaudel93@gmail.com>
2020-06-04 20:22:53 +0545

    tailwind added, animate.css added, skills and social medias added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/main.css'
+import 'animate.css'
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="grid grid-cols-3 gap-0 h-screen font-abeezee">
+    <div class="col-span-1 background-black text-white text-left p-10 pb-8">
+      <h1 class="text-8xl">Hello,</h1>
+      <h1 class="text-4xl">I am Prayog Paudel</h1>
+      <h1 class="text-4xl">Mandikhatar, KTM</h1>
+      <h1 class="text-4xl">+977-9840063248</h1>
+      <IconArray class="pt-10" />
+      <SocialMedias class="pt-20" />
+    </div>
+    <div class="col-span-2">
+      asdasddfgdfgdfg
+    </div>
   </div>
 </template>
 
 <script>
+import IconArray from '../components/icons'
+import SocialMedias from '../components/social-medias'
+
 export default {
   name: 'Home',
+  components: {
+    IconArray,
+    SocialMedias,
+  },
 }
 </script>
+
+<style lang="css">
+.background-black {
+  background-color: #020202;
+}
+.text-white {
+  color: #fff8f8;
+}
+</style>
